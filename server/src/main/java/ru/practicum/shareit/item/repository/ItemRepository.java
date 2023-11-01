91b166fb0b8b1590c05a3f89ddec3693bc7ca9e7
KoltsovaIA <igmtur@yandex.ru>
2023-11-01 20:50:01 +0300

    fix checkstyle

--- server/src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ server/src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -6,7 +6,6 @@ import ru.practicum.shareit.item.model.Item;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    List<Item> getAllByOwnerId(Long ownerId);
     List<Item> getAllByOwnerIdOrderByIdAsc(Long ownerId);
 
     List<Item> getAllByItemRequestId(Long requestId);
